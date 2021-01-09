5c81f1f59358e8b8b27ddbfceb3848de282833e1
Raksha <rakshapawar10.sae@gmail.com>
2021-01-09 12:11:33 +0530

    Trial change 2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
-import thunk from "redux-thunk";
+import thunkMiddleware from "redux-thunk";
 
 import "./index.css";
 import App from "./App";
@@ -14,7 +14,8 @@ import authReducer from "./store/reducers/auth";
 
 const composeEnhancers =
   process.env.REACT_APP_NODE_ENVX === "development"
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
@@ -24,7 +25,7 @@ const rootReducer = combineReducers({
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunkMiddleware))
 );
 
 const app = (
