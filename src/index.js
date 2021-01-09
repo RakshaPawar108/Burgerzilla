40257fc8f29b744047951c3309ef0781de80caf0
Raksha <rakshapawar10.sae@gmail.com>
2021-01-09 12:33:53 +0530

    New change made...will probably work?

--- src/index.js
+++ src/index.js
@@ -12,10 +12,12 @@ import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
 
+// const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
+
 const composeEnhancers =
-  process.env.REACT_APP_NODE_ENVX === "development"
+  (process.env.NODE_ENV === "development"
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
+    : null) || compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
